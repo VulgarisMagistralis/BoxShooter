9172869623e07e7c6d99289719c862c7bcfc3f64
VulgarisMagistralis <cenktuncbilek@gmail.com>
2022-02-12 17:22:55 -0500

    Initial upload

---
+++ Box Shooter/Assets/Scripts/Shooter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+public class Shooter : MonoBehaviour {
+	public GameObject projectile;
+	public Camera playerCamera;
+	public float power = 10.0f;
+	public AudioClip shootSFX;
+	void Update () {
+		// Detect if fire button is pressed
+		if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Jump")){	
+			// if projectile is specified
+			if(projectile){
+				// Instantiante projectile at the camera + 1 meter forward with camera rotation
+				GameObject newProjectile = Instantiate(projectile, playerCamera.transform.position + playerCamera.transform.forward, playerCamera.transform.rotation) as GameObject;
+				newProjectile.GetComponent<Rigidbody>().AddForce(playerCamera.transform.forward * power, ForceMode.VelocityChange);
+				if(shootSFX) AudioSource.PlayClipAtPoint(shootSFX, newProjectile.transform.position);				
+			}
+		}
+	}
+}
